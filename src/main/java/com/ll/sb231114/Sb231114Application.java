4ca59696c844c037a4ad0b860910b82fffed15ce
i-am-jiwon <dlark1128@gmail.com>
2023-11-14 09:24:28 +0900

    setting

---
+++ src/main/java/com/ll/sb231114/Sb231114Application.java
@@ -0,0 +1,13 @@
+package com.ll.sb231114;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb231114Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb231114Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sb231114/Sb231114ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sb231114;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb231114ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
