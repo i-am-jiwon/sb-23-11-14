52d0b21b078a2560b1b9d9702adf80753c1f7f91
i-am-jiwon <dlark1128@gmail.com>
2023-11-14 15:42:37 +0900

    article/write step2

--- src/main/java/com/ll/sb231114/ArticleController.java
+++ src/main/java/com/ll/sb231114/ArticleController.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ArticleController {
@@ -13,8 +14,9 @@ public class ArticleController {
     }
 
     @GetMapping("/article/doWrite")
+    @ResponseBody
     String doWrite(String title, String body) {
-        return "article/write";
+        return "게시물이 작성되었습니다.";
     }
 }
 
