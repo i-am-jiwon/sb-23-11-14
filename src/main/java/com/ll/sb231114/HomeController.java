40b3f971f9912eeec82aa36c664373f5ae05b883
i-am-jiwon <dlark1128@gmail.com>
2023-11-14 13:27:33 +0900

    calc4

--- src/main/java/com/ll/sb231114/HomeController.java
+++ src/main/java/com/ll/sb231114/HomeController.java
@@ -2,6 +2,7 @@ package com.ll.sb231114;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -17,5 +18,35 @@ public class HomeController {
     @ResponseBody
     String showAbout(){
         return "개발자 커뮤니티";
+    }
+
+    @GetMapping("/calc")
+    @ResponseBody
+    String showCalc(int a, int b){
+        return "계산결과 %d".formatted(a+b);
+    }
+
+    @GetMapping("/calc2")
+    @ResponseBody
+    String showCalc2(Integer a, Integer b){
+        return "a %d b %d".formatted(a,b);
+    }
+
+    @GetMapping("/calc3")
+    @ResponseBody
+    String showCalc3(
+            @RequestParam(defaultValue = "0") double a,
+            @RequestParam(defaultValue = "0") double b
+    ){
+        return "a + b %f".formatted(a+b);
+    }
+
+    @GetMapping("/calc4")
+    @ResponseBody
+    String showCalc4(
+            @RequestParam(defaultValue = "0") String a,
+            @RequestParam(defaultValue = "0") String b
+    ){
+        return "a + b %s".formatted(a+b);
     }
 }
