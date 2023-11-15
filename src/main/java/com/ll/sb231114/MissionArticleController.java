f3ad63d9a54c2663ac75c6bf320dfe1f837e78c2
i-am-jiwon <dlark1128@gmail.com>
2023-11-15 10:13:57 +0900

    mission

---
+++ src/main/java/com/ll/sb231114/MissionArticleController.java
@@ -0,0 +1,65 @@
+package com.ll.sb231114;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class MissionArticleController {
+
+    private List<Article> articles = new ArrayList<>();
+
+    @GetMapping("/article/write")
+    String write() {
+        return "article/write";
+    }
+
+    @GetMapping("/article/doWrite")
+    @ResponseBody
+    RsData<Article> doWrite(String title, String body) {
+        Article article = new Article(articles.size()+1, title, body);
+        articles.add(article);
+
+        RsData<Article> rs = new RsData(
+                "S-1",
+                "%d번 게시물 작성".formatted(article.getId()),
+                article
+        );
+        return rs;
+    }
+
+    @GetMapping("/article/getLastArticle")
+    @ResponseBody
+    Article getLastArticle(){
+        return articles.getLast();
+    }
+
+    @GetMapping("/article/getArticles")
+    @ResponseBody
+    List<Article> getArticles(){
+        return articles;
+    }
+
+}
+
+
+@AllArgsConstructor
+@Getter
+class RsData<T> {
+    private String resultCode;
+    private String msg;
+    private T data;
+}
+
+@AllArgsConstructor
+@Getter
+class Article {
+    private long id;
+    private String title;
+    private String body;
+}
