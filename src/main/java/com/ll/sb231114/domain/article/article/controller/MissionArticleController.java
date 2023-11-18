c7c2f823634932351166aaf464e189927a9850f8
i-am-jiwon <dlark1128@gmail.com>
2023-11-19 00:12:46 +0900

    clear

--- src/main/java/com/ll/sb231114/domain/article/article/controller/MissionArticleController.java
+++ src/main/java/com/ll/sb231114/domain/article/article/controller/MissionArticleController.java
@@ -2,9 +2,6 @@ package com.ll.sb231114.domain.article.article.controller;
 
 import com.ll.sb231114.domain.article.article.entity.Article;
 import com.ll.sb231114.domain.article.article.service.ArticleService;
-import com.ll.sb231114.global.rq.Rq;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -15,7 +12,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -26,8 +22,6 @@ public class MissionArticleController {
 
 
     private final ArticleService articleService;
-
-    private final Rq rq;
 
 
     @GetMapping("/article/detail/{id}")
@@ -96,53 +90,6 @@ public class MissionArticleController {
         String msg = "%d".formatted(article.getId());
         return "redirect:/article/list?msg=" + msg;
     }
-
-
-    @GetMapping("/article/getLastArticle")
-    @ResponseBody
-    Article getLastArticle() {
-        return articleService.findLastArticle();
-    }
-
-    @GetMapping("/article/getArticles")
-    @ResponseBody
-    List<Article> getArticles() {
-        return articleService.findAll();
-    }
-
-    @GetMapping("/article/articleServicePointer")
-    @ResponseBody
-    String articleServicePointer() {
-        return articleService.toString();
-    }
-
-    @GetMapping("/article/httpServletRequestPointer")
-    @ResponseBody
-    String httpServletRequestPointer(HttpServletRequest req) {
-        return req.toString();
-    }
-
-    @GetMapping("/article/httpServletResponsePointer")
-    @ResponseBody
-    String httpServletResponsePointer(HttpServletResponse resp) {
-        return resp.toString();
-    }
-
-    @GetMapping("/article/rqPointer")
-    @ResponseBody
-    String rqPointer() {
-        return rq.toString();
-    }
-
-    @GetMapping("/article/rqTest")
-    String showRqTest(Model model) {
-        String rqToString = rq.toString();
-
-        model.addAttribute("rqToString", rqToString);
-
-        return "article/rqTest";
-    }
-
 
     @GetMapping("/article/list")
     String showList(Model model) {
