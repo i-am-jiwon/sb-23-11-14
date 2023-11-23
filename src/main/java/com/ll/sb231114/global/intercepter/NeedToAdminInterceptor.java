968404b4737953a33cab21d5d9346bd11a85e4dd
i-am-jiwon <dlark1128@gmail.com>
2023-11-23 22:42:06 +0900

    part3 step39

--- src/main/java/com/ll/sb231114/global/intercepter/NeedToAdminInterceptor.java
+++ src/main/java/com/ll/sb231114/global/intercepter/NeedToAdminInterceptor.java
@@ -1,15 +1,26 @@
 package com.ll.sb231114.global.intercepter;
 
+import com.ll.sb231114.global.rq.Rq;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
+@RequiredArgsConstructor
 public class NeedToAdminInterceptor implements HandlerInterceptor {
+
+    private final Rq rq;
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        System.out.println("정말로 내가 자동으로 실행되나?");
+        if (!rq.isLogined()){
+            throw new RuntimeException("please login");
+        }
+
+        if (!rq.isAdmin()){
+            throw new RuntimeException("no admin");
+        }
 
         return true;
     }
