1c8c8a10cf608372092ee82ab14c306ebbe510f1
i-am-jiwon <dlark1128@gmail.com>
2023-11-23 22:39:52 +0900

    part3 step38

--- src/main/java/com/ll/sb231114/global/intercepter/NeedToAdminInterceptor.java
+++ src/main/java/com/ll/sb231114/global/intercepter/NeedToAdminInterceptor.java
@@ -2,9 +2,11 @@ package com.ll.sb231114.global.intercepter;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-public class NeedTodAdminInterceptor implements HandlerInterceptor {
+@Component
+public class NeedToAdminInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         System.out.println("정말로 내가 자동으로 실행되나?");
---
+++ src/main/java/com/ll/sb231114/global/webMvc/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.ll.sb231114.global.webMvc;
+
+import com.ll.sb231114.global.intercepter.NeedToAdminInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final NeedToAdminInterceptor needToAdminInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(needToAdminInterceptor)
+                .addPathPatterns("/adm/**");
+    }
+}
