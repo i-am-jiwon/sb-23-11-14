6d8ed2c1fe93f6a5f8ac102e826952614622233e
i-am-jiwon <dlark1128@gmail.com>
2023-11-24 17:02:51 +0900

    part 4 step 14

--- src/main/java/com/ll/sb231114/global/rq/Rq.java
+++ src/main/java/com/ll/sb231114/global/rq/Rq.java
@@ -2,13 +2,21 @@ package com.ll.sb231114.global.rq;
 
 import com.ll.sb231114.domain.member.member.entity.Member;
 import com.ll.sb231114.domain.member.member.service.MemberService;
+import com.ll.sb231114.domain.security.SecurityConfig;
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.boot.actuate.endpoint.SecurityContext;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
+import java.net.Authenticator;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
@@ -22,7 +30,17 @@ public class Rq {
     private final HttpServletRequest req;
     private final HttpServletResponse resp;
     private final MemberService memberService;
+    private User user;
     private Member member;
+
+    @PostConstruct
+    public void init() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication.getPrincipal() instanceof User) {
+            this.user = (User) authentication.getPrincipal();
+        }
+    }
 
     public String redirect(String path, String msg) {
         msg = URLEncoder.encode(msg, StandardCharsets.UTF_8);
@@ -30,27 +48,24 @@ public class Rq {
         return "redirect:" + path + "?msg=" + msg;
     }
 
-    private long getMemberId() {
-        return Optional
-                .ofNullable(req.getSession().getAttribute("loginedMemberId"))
-                .map(id -> (long) id)
-                .orElse(0L);
+    private String getMemberUsername() {
+        return user.getUsername();
     }
 
     public Member getMember() {
-        if(!isLogined()){
+        if (!isLogined()) {
             return null;
         }
 
-        if(member == null){
-            member = memberService.findById(getMemberId()).get();
+        if (member == null) {
+            member = memberService.findByUsername(getMemberUsername()).get();
         }
 
         return member;
     }
 
     public boolean isLogined() {
-        return getMemberId() > 0;
+        return user != null;
     }
 
     public void setSessionAttr(String name, Object value) {
@@ -58,7 +73,7 @@ public class Rq {
     }
 
     public <T> T getSessionAttr(String name) {
-       return (T) req.getSession().getAttribute(name);
+        return (T) req.getSession().getAttribute(name);
     }
 
     public void removeSessionAttr(String name) {
@@ -66,6 +81,7 @@ public class Rq {
     }
 
     public boolean isAdmin() {
-        return getMember().isAdmin();
+        return user.getAuthorities()
+                .stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
     }
 }
