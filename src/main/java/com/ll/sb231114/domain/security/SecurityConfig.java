6efecd5052a7c7d9dd71e5efad7bc74d94c254c4
i-am-jiwon <dlark1128@gmail.com>
2023-11-24 16:02:30 +0900

    part 4 step 12

--- src/main/java/com/ll/sb231114/domain/security/SecurityConfig.java
+++ src/main/java/com/ll/sb231114/domain/security/SecurityConfig.java
@@ -3,27 +3,28 @@ package com.ll.sb231114.domain.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         return http
-                .authorizeHttpRequests(
-                        authorizeRequests -> authorizeRequests
-                                .anyRequest().permitAll()
-                )
                 .formLogin(
                         formLogin -> formLogin
                                 .loginPage("/member/login")
                                 .loginProcessingUrl("/member/login")
                                 .usernameParameter("username")
-                                .usernameParameter("password")
+                                .passwordParameter("password")
                                 .defaultSuccessUrl("/article/list")
-                                .permitAll()
-                        )
+                )
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
