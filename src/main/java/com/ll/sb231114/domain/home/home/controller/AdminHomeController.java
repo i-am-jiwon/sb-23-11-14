e62e2cfd86bc072febfea4f18811be7501030646
i-am-jiwon <dlark1128@gmail.com>
2023-11-26 00:07:45 +0900

    part5 step 6

--- src/main/java/com/ll/sb231114/domain/home/home/controller/AdminHomeController.java
+++ src/main/java/com/ll/sb231114/domain/home/home/controller/AdminHomeController.java
@@ -4,19 +4,21 @@ import com.ll.sb231114.global.rq.Rq;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/adm")
 public class AdminHomeController {
     private final Rq rq;
 
-    @GetMapping("/adm")
+    @GetMapping("")
     public String showMain(){
 
         return "home/home/adm/main";
     }
 
-    @GetMapping("/adm/home/about")
+    @GetMapping("/home/about")
     public String showAbout(){
 
         return "home/home/adm/main";
