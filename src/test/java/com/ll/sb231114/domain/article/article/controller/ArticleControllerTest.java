87f175a03a8026202d5e78938da47ad1a9c1ad1b
i-am-jiwon <dlark1128@gmail.com>
2023-11-26 19:28:03 +0900

    part5 step23

--- src/test/java/com/ll/sb231114/domain/article/article/controller/ArticleControllerTest.java
+++ src/test/java/com/ll/sb231114/domain/article/article/controller/ArticleControllerTest.java
@@ -1,5 +1,8 @@
 package com.ll.sb231114.domain.article.article.controller;
 
+
+import com.ll.sb231114.domain.article.article.entity.Article;
+import com.ll.sb231114.domain.article.article.service.ArticleService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +12,19 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class ArticleControllerTest {
     @Autowired
     private MockMvc mvc;
+    @Autowired
+    private ArticleService articleService;
 
     // GET /article/list
     @Test
@@ -26,8 +34,50 @@ public class ArticleControllerTest {
         ResultActions resultActions = mvc
                 .perform(get("/article/list"))
                 .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(handler().handlerType(MissionArticleController.class))
+                .andExpect(handler().methodName("showList"))
+                .andExpect(content().string(containsString("""
+                        글 목록
+                        """.stripIndent().trim())))
+                .andExpect(content().string(containsString("""
+                        3번 : member2
+                        """.stripIndent().trim())))
+                .andExpect(content().string(containsString("""
+                        2번 : member1
+                        """.stripIndent().trim())))
+                .andExpect(content().string(containsString("""
+                        1번 : admin
+                        """.stripIndent().trim())));
     }
 
+    @Test
+    @DisplayName("게시물 내용 페이지를 보여준다")
+    void t2() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(get("/article/detail/1"))
+                .andDo(print());
+
+        Article article = articleService.findById(1L).get();
+
+        // THEN
+        resultActions
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(handler().handlerType(MissionArticleController.class))
+                .andExpect(handler().methodName("showDetail"))
+                .andExpect(content().string(containsString("""
+                        글 내용
+                        """.stripIndent().trim())))
+                .andExpect(content().string(containsString("""
+                        <div class="badge badge-outline">1</div>
+                        """.stripIndent().trim())))
+                .andExpect(content().string(containsString(article.getTitle())))
+                .andExpect(content().string(containsString(article.getBody())));
+    }
 
     // GET /article/detail/{id}
     // GET /article/write
