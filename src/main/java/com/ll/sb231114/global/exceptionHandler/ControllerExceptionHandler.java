3eb79e4fc4d07a693f0dcf18d97d09cb8565c02c
i-am-jiwon <dlark1128@gmail.com>
2023-11-28 23:17:04 +0900

    part6 step18

--- src/main/java/com/ll/sb231114/global/exceptionHandler/ControllerExceptionHandler.java
+++ src/main/java/com/ll/sb231114/global/exceptionHandler/ControllerExceptionHandler.java
@@ -3,10 +3,12 @@ package com.ll.sb231114.global.exceptionHandler;
 
 import com.ll.sb231114.global.rq.Rq;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Profile("prod")
 @ControllerAdvice(annotations = Controller.class)
 @RequiredArgsConstructor
 public class ControllerExceptionHandler {
