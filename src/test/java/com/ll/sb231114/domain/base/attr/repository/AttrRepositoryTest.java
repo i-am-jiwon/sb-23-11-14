5a3bfa8862bb47bb584d305fd39f32d2a9cc98ac
i-am-jiwon <dlark1128@gmail.com>
2023-11-28 23:02:12 +0900

    part6 step13

--- src/test/java/com/ll/sb231114/domain/base/attr/repository/AttrRepositoryTest.java
+++ src/test/java/com/ll/sb231114/domain/base/attr/repository/AttrRepositoryTest.java
@@ -1,31 +1,53 @@
 package com.ll.sb231114.domain.base.attr.repository;
 
-import com.ll.sb231114.domain.article.article.repository.ArticleRepository;
 import com.ll.sb231114.domain.base.attr.entity.Attr;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@ActiveProfiles("attr저장")
+@ActiveProfiles("test")
 public class AttrRepositoryTest {
     @Autowired
     private AttrRepository attrRepository;
 
-    @DisplayName("t1")
+    @DisplayName("attr저장")
     @Test
-    void t1(){
+    @Transactional
+    @Rollback
+    void t1() {
         Attr attr = Attr.builder()
                 .createDate(LocalDateTime.now())
                 .name("Age")
                 .build();
         attrRepository.save(attr);
         assertThat(attr.getId()).isGreaterThan(0L);
+    }
+
+    @DisplayName("attr저장 한 번 더")
+    @Test
+    @Transactional
+    @Rollback
+    void t2() {
+        Attr attr = Attr.builder()
+                .createDate(LocalDateTime.now())
+                .name("Age")
+                .build();
+        attrRepository.save(attr);
+        assertThat(attr.getId()).isGreaterThan(0L);
+    }
+
+    @DisplayName("attr select count(*) from attr")
+    @Test
+    void t3() {
+        assertThat(attrRepository.count()).isEqualTo(0);
     }
 }
