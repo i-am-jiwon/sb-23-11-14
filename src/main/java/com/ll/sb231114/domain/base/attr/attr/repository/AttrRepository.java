4a082cc521f4c98326e9a40fe35184aaf49f85cd
i-am-jiwon <dlark1128@gmail.com>
2023-11-28 22:54:41 +0900

    part6 step11

--- src/main/java/com/ll/sb231114/domain/base/attr/attr/repository/AttrRepository.java
+++
@@ -1,4 +0,0 @@
-package com.ll.sb231114.domain.base.attr.attr.repository;
-
-public class AttrRepository {
-}
--- src/test/java/com/ll/sb231114/domain/base/attr/repository/AttrRepositoryTest.java
+++ src/test/java/com/ll/sb231114/domain/base/attr/repository/AttrRepositoryTest.java
@@ -1,8 +1,10 @@
 package com.ll.sb231114.domain.base.attr.repository;
 
+import com.ll.sb231114.domain.article.article.repository.ArticleRepository;
 import com.ll.sb231114.domain.base.attr.entity.Attr;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -11,8 +13,11 @@ import java.time.LocalDateTime;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@ActiveProfiles("attr저장")
 public class AttrRepositoryTest {
+    @Autowired
+    private AttrRepository attrRepository;
+
     @DisplayName("t1")
     @Test
     void t1(){
